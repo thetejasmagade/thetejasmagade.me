9a2d6ea0e5cbe455158d13f12674ac84b22b7552
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2023-01-06 22:36:44 +0530

    fixed: padding

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="intro py-12 px-9 sm:py-7 sm:px-40">
+  <div class="intro py-12 px-8 sm:py-7 sm:px-40">
     <h2 class="text-[50px] sm:text-[90px] font-bold flex">Hello. 
       <span>
         <img class="hello_gif sm:h-24 sm:w-24 h-[50px] w-[50px]"
           src="https://camo.githubusercontent.com/e8e7b06ecf583bc040eb60e44eb5b8e0ecc5421320a92929ce21522dbc34c891/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6876524a434c467a6361737252346961377a2f67697068792e676966">
         </span>
     </h2>
-    <h3 class="text-[22px] sm:text-[34px] pt-4">My name is <span class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#00dc82] to-[#36e4da]">Tejas Magade</span></h3>
+    <h3 class="text-[23px] sm:text-[34px] pt-4">My name is <span class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#00dc82] to-[#36e4da]">Tejas Magade</span></h3>
   </div>
 </template>
 
