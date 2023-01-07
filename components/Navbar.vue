8ec8f6f3e208306b7c6b860a7090da354be93b4a
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2023-01-07 10:05:45 +0530

    added: pages in navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,8 +4,25 @@
 <template>
   <header>
     <div class="flex justify-between items-center max-w-8xl mx-auto mx-auto px-8 sm:py-1 sm:px-40 py-1">
-        <!-- <img src="~/assets/signature.svg" height="50px" width="50px"> -->
-        <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" version="1.0" preserveAspectRatio="xMidYMid meet" viewBox="331.4 40.22 155.53 83.78"><g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)" :fill="$colorMode.value === 'dark' ? '#ffffff' : '#000000'" stroke="none"><path d="M4350 1593 c-317 -35 -892 -158 -1003 -214 -39 -20 -40 -22 -21 -36 25 -19 86 -12 262 29 73 17 135 28 139 25 3 -4 -38 -102 -92 -219 -135 -293 -163 -380 -131 -407 19 -16 27 -1 112 220 113 295 201 444 277 468 33 10 303 60 442 81 80 13 113 29 101 49 -7 11 -18 11 -86 4z"></path><path d="M4738 1204 c-46 -31 -138 -99 -205 -150 -68 -52 -127 -94 -133 -94 -15 0 -12 72 7 171 9 49 14 92 10 95 -12 13 -54 -14 -152 -96 -107 -90 -129 -89 -76 3 30 53 41 107 21 107 -16 0 -71 -93 -159 -269 -72 -144 -81 -166 -68 -178 28 -28 59 1 187 177 74 102 127 150 163 150 22 0 23 4 -17 -144 -30 -112 -33 -164 -11 -172 9 -4 52 29 117 88 195 176 324 278 355 278 22 0 15 -23 -48 -154 -69 -142 -80 -190 -54 -230 10 -14 21 -26 25 -26 4 0 10 25 14 55 8 71 44 165 106 280 51 93 60 131 38 153 -20 20 -33 15 -120 -44z"></path></g></svg>
+      <!-- <img src="~/assets/signature.svg" height="50px" width="50px"> -->
+      <BaseLogo />
+      <div class="flex items-center">
+        <nav aria-label="Main Menu" class="opacity-75">
+          <ul class="flex sm:space-x-10 space-x-1 items-center">
+            <li>
+              <NuxtLink to="#" class="hover:text-[#01DC84]">
+                <span class="inline-block pr-2 invisible sm:visible">
+                <svg width="18" height="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M7 18h7m-7-4h1m-1-4h3M7 2h9.5L21 6.5V19"/><path d="M3 20.5v-14A1.5 1.5 0 0 1 4.5 5h9.752a.6.6 0 0 1 .424.176l3.148 3.148A.6.6 0 0 1 18 8.75V20.5a1.5 1.5 0 0 1-1.5 1.5h-12A1.5 1.5 0 0 1 3 20.5Z"/><path d="M14 5v3.4a.6.6 0 0 0 .6.6H18"/></g></svg>
+              </span>Blog</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="#" class="hover:text-[#01DC84]"><span class="inline-block pr-2 invisible sm:visible">
+                <svg width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M1 11h3v2H1v-2m18.1-7.5L17 5.6L18.4 7l2.1-2.1l-1.4-1.4M11 1h2v3h-2V1M4.9 3.5L3.5 4.9L5.6 7L7 5.6L4.9 3.5M10 22c0 .6.4 1 1 1h2c.6 0 1-.4 1-1v-1h-4v1m2-16c-3.3 0-6 2.7-6 6c0 2.2 1.2 4.2 3 5.2V19c0 .6.4 1 1 1h4c.6 0 1-.4 1-1v-1.8c1.8-1 3-3 3-5.2c0-3.3-2.7-6-6-6m1 9.9V17h-2v-1.1c-1.7-.4-3-2-3-3.9c0-2.2 1.8-4 4-4s4 1.8 4 4c0 1.9-1.3 3.4-3 3.9m7-4.9h3v2h-3v-2Z"/></svg>
+              </span>Projects</NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 </template>
