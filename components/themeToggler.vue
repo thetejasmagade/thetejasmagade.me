733bc68751eb87004ac8adda524fdbf3b878e6f7
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2023-01-07 10:58:39 +0530

    added: sepia mode

--- components/themeToggler.vue
+++ components/themeToggler.vue
@@ -17,6 +17,8 @@
                 </path>
             </svg>
 
+            <svg v-else-if="$colorMode.preference === 'sepia'" class="icon" width="2em" height="2em" viewBox="0 0 256 256"><path fill="currentColor" d="M80 56V24a8 8 0 0 1 16 0v32a8 8 0 0 1-16 0Zm40 8a8 8 0 0 0 8-8V24a8 8 0 0 0-16 0v32a8 8 0 0 0 8 8Zm32 0a8 8 0 0 0 8-8V24a8 8 0 0 0-16 0v32a8 8 0 0 0 8 8Zm96 56v8a40 40 0 0 1-37.5 39.9a98 98 0 0 1-27 40.1H208a8 8 0 0 1 0 16H32a8 8 0 0 1 0-16h24.5A96.4 96.4 0 0 1 24 136V88a8 8 0 0 1 8-8h176a40 40 0 0 1 40 40Zm-48-24H40v40a80.3 80.3 0 0 0 45.1 72h69.8a80.3 80.3 0 0 0 41.9-49.9a1.4 1.4 0 0 0 .2-.6a77 77 0 0 0 3-21.5Zm32 24a24 24 0 0 0-16-22.6V136a92.9 92.9 0 0 1-1.2 15a24 24 0 0 0 17.2-23Z"/></svg>
+
             <svg v-else xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
                 role="img" class="icon" width="2em" height="2em" viewBox="0 0 24 24" data-v-389e99a3=""
                 data-v-97ee44f7="">
@@ -37,6 +39,9 @@ function changeTheme() {
         colorMode.preference = 'light'
     }
     else if (colorMode.preference === 'light') {
+        colorMode.preference = 'sepia'
+    }
+    else if (colorMode.preference === 'sepia') {
         colorMode.preference = 'system'
     }
     else if (colorMode.preference === 'system') {
